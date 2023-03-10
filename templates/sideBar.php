a7ffadb5690dedc0997b06a3d0863002546b1f43
HusseinSharifH <126942911+HusseinSharifH@users.noreply.github.com>
2023-03-11 00:09:34 +0300

    Add files via upload

---
+++ templates/sideBar.php
@@ -0,0 +1,29 @@
+<aside>
+    <img src="img/logo.jpg" alt="Logo" class="logo">
+    <ul>
+        <a href="#">
+            <i class="fas fa-home"></i>
+            <li>الرئيسية</li>
+        </a>
+        
+        <a href="#" class="active">
+            <i class="fas fa-layer-group"></i>
+            <li>المنتجات</li>
+        </a>
+        
+        <a href="#">
+            <i class="fas fa-box"></i>
+            <li>البوكس</li>
+        </a>
+        
+        <a href="#">
+            <i class="fas fa-user-friends"></i>
+            <li>المندوبين</li>
+        </a>
+        
+        <a href="#">
+            <i class="fas fa-users-cog"></i>
+            <li>المشرفين</li>
+        </a>
+    </ul>
+</aside>
